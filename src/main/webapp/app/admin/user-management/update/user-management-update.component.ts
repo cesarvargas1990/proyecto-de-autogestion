77bc66ea8f9ca5229975ef3df9f6392b7133344b
joan.caicedo@supergiros.com.co <joan.caicedo@supergiros.com.co>
2022-04-13 09:45:41 -0500

    Se transcriben los cambios del bugfix 25624 mejora-visual para acoplar con develop

--- src/main/webapp/app/admin/user-management/update/user-management-update.component.ts
+++ src/main/webapp/app/admin/user-management/update/user-management-update.component.ts
@@ -8,14 +8,13 @@ import { UserManagementService } from '../service/user-management.service';
 import { GrillaManagementService } from '../service/grilla-management.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { loadingModalComponent } from '../modal/loadingModal.component';
-
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'jhi-user-mgmt-update',
   templateUrl: './user-management-update.component.html',
 })
 export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
-
   user!: User;
 
   udmmodel!: udmModel;
@@ -27,6 +26,7 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
   municipiosListNameFull: string[] = [];
 
   municipiosListIDFull: number[] = [];
+  municipiosListNameFullEmpty: string[] = [];
 
   convenioNameEdit: string[] = [];
   programaNameEdit: string[] = [];
@@ -52,23 +52,32 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
   convenioName!: string;
   programaName!: string;
 
+  lengthDepartamentoList!: number;
+
   idDepartamentos: number[] = [];
   idDepartamentos2: number[] = [];
   nameDepartamentos: string[] = [];
   nameDepartamentos2: string[] = [];
 
-
   idConvenio!: number;
   validadorCelphone!: number;
   validadorEmail!: string;
+
+  dropdownList: string[] = [];
+  selectedItems: string[] = [];
+  dropdownSettings = {};
+  dropdownSettingsdocumenttypes = {};
+  dropdownSettingsDepartamento = {};
 
   isSaving = false;
   celphoneCredentialsError = false;
   emailCredentialsError = false;
   idCredentialsError = false;
+  idMunicipiosError = false;
   addMunicipiosVerification = true;
   addLocationVerification = true;
   addLocationVerification2 = true;
+  EditOrCreate = true;
 
   saveReady = true;
   verificationMunicipiosFinish = false;
@@ -99,7 +108,7 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
     private route: ActivatedRoute,
     private fb: FormBuilder,
     private modalService: NgbModal
-  ) { }
+  ) {}
 
   ngAfterViewInit(): void {
     this.route.data.subscribe(({ user }) => {
@@ -112,12 +121,14 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     this.route.data.subscribe(({ user }) => {
+      this.departamentoName = this.municipiosListNameFull.toString();
       if (user) {
         this.user = user;
         if (this.user.id === undefined) {
           this.user.activated = true;
+          this.EditOrCreate = true;
         } else {
-
+          this.EditOrCreate = false;
 
           this.userService.getDepartamentosName(this.user.id).subscribe(x => {
             this.departamentosListNameFull = x;
@@ -139,6 +150,7 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
               });
             }
             this.departamentoName = this.departamentosListNameFull.toString();
+            this.selectedItems = this.departamentosListNameFull;
             this.editForm.patchValue({
               departamento: this.departamentosListNameFull,
             });
@@ -149,8 +161,6 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
             this.editForm.patchValue({
               municipio: this.municipiosListNameFull,
             });
-
-
           });
 
           this.userService.findConvenioID(this.user.id).subscribe(x => {
@@ -160,17 +170,53 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
               this.convenioName = 'DPS - Departamento para la Prosperidad Social';
             });
           });
-
         }
       }
     });
     this.addLocationVerification = true;
     this.addLocationVerification2 = true;
     this.addMunicipiosVerification = true;
-
-    this.userService.getDepartamentos().subscribe(departamentosName => (this.departamento = departamentosName));
+    this.userService.getDepartamentos().subscribe(departamentosName => {
+      this.departamento = departamentosName;
+      this.lengthDepartamentoList = departamentosName.length;
+      this.dropdownList = departamentosName;
+    });
     this.userService.authorities().subscribe(authorities => (this.authorities = authorities));
     this.userService.getConvenios().subscribe(conveniosName => (this.convenio = conveniosName));
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Seleccionar Todos',
+      unSelectAllText: 'Limpiar busqueda',
+      itemsShowLimit: 0,
+      allowSearchFilter: true,
+      noDataAvailablePlaceholderText: 'No hay información disponible',
+      // enableCheckAll: false
+    };
+    this.dropdownSettingsdocumenttypes = {
+      singleSelection: true,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 0,
+      allowSearchFilter: true,
+      enableCheckAll: false,
+      noDataAvailablePlaceholderText: 'No hay información disponible',
+    };
+    this.dropdownSettingsDepartamento = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 0,
+      allowSearchFilter: true,
+      enableCheckAll: false,
+      noDataAvailablePlaceholderText: 'No hay información disponible',
+    };
   }
 
   previousState(): void {
@@ -226,8 +272,7 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
   }
 
   addTime(): void {
-    const a = "1";
-
+    const a = '1';
   }
 
   private updateForm(user: User): void {
@@ -249,7 +294,7 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
 
   private updateUser(user: User): void {
     user.login = this.editForm.get(['login'])!.value;
-    user.documentType = this.editForm.get(['documentType'])!.value;
+    user.documentType = this.editForm.get(['documentType'])!.value.toString();
     this.validadorCelphone = this.editForm.get(['celphone'])!.value;
     if (this.validadorCelphone.toString().startsWith('3')) {
       user.celphone = this.editForm.get(['celphone'])!.value;
@@ -272,19 +317,24 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
   }
 
   private updateDepartamento(user: User): void {
-    const ad = this.editForm.get(['departamento'])!.value.toString();
-    this.departamentoName = ad;
-    this.userService.getIdMultiplesDepartamentos(ad).subscribe(xx => {
-      //MUESTRA LOS MUNICIPIOS EN EL FRONTEND
-      this.addMunicipiosVerification = false;
+    let ad = this.editForm.get(['departamento'])!.value;
 
-      this.nameDepartamentos = ad;
-      this.idDepartamentos = xx; //Lista tamaño n con id de departamentos
-      this.userService.getMultiplesMunicipios(this.idDepartamentos).subscribe(xxx => {
-        this.municipio = xxx; this.addMunicipiosVerification = true;
+    //Se selecciona -TODOS-
+    if (ad.indexOf('-TODOS-') >= 0) {
+      ad = '-TODOS-';
+      this.municipio = [];
+      this.editForm.patchValue({
+        municipio: [],
       });
+      this.departamentoName = ad.toString();
+      this.municipioName = '';
+    } else {
+      this.departamentoName = ad.toString();
+    }
 
-    });
+    //Se vuelve a dar "agregar departamento" despúes de dar "agregar municipio" e interactuar, hay dos opciones:
+    //1. Se agrega un dep que no estaba           ->  Actualizar lista municipios disponible y mantiene los seleccionados de los deps que estaban
+    //2. se cambian totalmente el departamento    ->  Actualizar lista municipios disponibles y borra los seleccionados de los dep anteriores
     if (this.departamentosListFull.length !== 0) {
       this.departamentosListFull = [];
       this.municipiosListFull = [];
@@ -292,8 +342,27 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
       this.saveReady = true;
     }
 
-  }
+    this.userService.getIdMultiplesDepartamentos(ad).subscribe(xx => {
+      //MUESTRA LOS MUNICIPIOS EN EL FRONTEND
+      this.addMunicipiosVerification = false;
+      this.nameDepartamentos = ad;
+      this.idDepartamentos = xx; //Lista tamaño n con id de departamentos
+      this.userService.getMultiplesMunicipios(this.idDepartamentos).subscribe(xxx => {
+        this.municipio = xxx;
+        this.addMunicipiosVerification = true;
 
+        if (this.EditOrCreate === false) {
+          this.userService.findMunicipiosNameByIDAndDepartamento(this.user.id!, this.idDepartamentos).subscribe(x => {
+            this.municipiosListNameFullEmpty = [];
+            this.municipiosListNameFullEmpty = x;
+            this.editForm.patchValue({
+              municipio: this.municipiosListNameFullEmpty,
+            });
+          });
+        }
+      });
+    });
+  }
 
   private addDepartamentoAndMunicipio(user: User): void {
     const ad = this.editForm.get(['municipio'])!.value;
@@ -301,19 +370,27 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
     this.municipioName = ad.toString();
     this.idDepartamentos2 = [];
     this.addLocationVerification2 = false;
-    //AQUI SE INICIA EL MODAL
-    this.modalService.open(loadingModalComponent);
+    if (this.municipio.length === ad.length) {
+      this.municipioName = 'Todos los Municipios de ' + this.departamentoName;
+    } else {
+      this.municipioName = ad.toString();
+    }
 
+    if (ad.length !== 0) {
+      //AQUI SE INICIA EL MODAL
+      this.modalService.open(loadingModalComponent);
 
-    for (let index = 0; index < ad.length; index++) {
-      this.userService.findDepartamentosIDByMunicipioName(ad[index]).subscribe(x => {
-        this.departamentosListFull.push(x);
-      });
+      for (let index = 0; index < ad.length; index++) {
+        this.userService.findDepartamentosIDByMunicipioName(ad[index]).subscribe(x => {
+          this.departamentosListFull.push(x);
+        });
 
-      this.userService.getIdMunicipios(ad[index])
-        .subscribe({
+        this.userService.getIdMunicipios(ad[index]).subscribe({
           next: xx => {
             this.municipiosListId[index] = xx.toString();
+            /* eslint-disable */
+            /* eslint-enable */
+
             if (index === ad.length - 1) {
               this.verificationMunicipiosFinish = true;
               this.addLocationVerification = false;
@@ -323,18 +400,14 @@ export class UserManagementUpdateComponent implements OnInit, AfterViewInit {
             }
           },
         });
-    }
-    if (ad.length === this.municipio.length) {
-      this.municipioName = 'Todos los Municipios';
+      }
+      this.idMunicipiosError = false;
     } else {
-      user.departamentos = this.departamentoName;
+      this.idMunicipiosError = true;
     }
 
     if (this.municipioName === '-TODOS-') {
       this.addLocationVerification = true;
-      this.saveReady = false;
-      this.departamentosListFull = ['1000'];
-      this.municipiosListFull = ['1'];
     } else {
       if (this.verificationMunicipiosFinish === true) {
         this.saveReady = true;
